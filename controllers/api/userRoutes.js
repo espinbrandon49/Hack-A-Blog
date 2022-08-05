7233090acb417649476f9898be62d675d9fb1fef
Brandon Espinosa <espinbrandon49@gmail.com>
2022-08-04 21:45:06 -0700

    add front-end, client js blog create

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { User } = require('../../models');
-
+//endpoint routes
+//signup
 router.post('/', async (req, res) => {
   try {
     const userData = await User.create({
@@ -18,6 +19,7 @@ router.post('/', async (req, res) => {
   }
 });
 
+//login
 router.post('/login', async (req, res) => {
   try {
     const userData = await User.findOne({ where: { username: req.body.username } });
@@ -50,6 +52,7 @@ router.post('/login', async (req, res) => {
   }
 });
 
+//logout
 router.post('/logout', (req, res) => {
   if (req.session.logged_in) {
     req.session.destroy(() => {
@@ -60,10 +63,4 @@ router.post('/logout', (req, res) => {
   }
 });
 
-router.get('/signup', (req, res) => {
-  res.render('signup');
-});
-
-
 module.exports = router;
-
