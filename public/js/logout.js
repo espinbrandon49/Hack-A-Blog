7c7b77706ff2ed904ce30b71d2cb8b463e8414d5
Brandon Espinosa <espinbrandon49@gmail.com>
2022-08-14 18:04:58 -0700

    add idle logout timer

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,5 +1,4 @@
 const logout = async () => {
-  console.log('meow')
   const response = await fetch('/api/users/logout', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
@@ -12,4 +11,41 @@ const logout = async () => {
   }
 };
 
+let timer;
+
+const runTimer = () => {
+  timer = setTimeout(() => {
+    logout()
+  }, 60000)
+}
+//runTimer();
+
+function resetLogout() {
+  clearTimeout(myTimeout)
+}
+
+window.addEventListener('click', (e) => {
+  clearTimeout(timer)
+  runTimer()
+})
+
+window.addEventListener('mousemove', (e) => {
+  clearTimeout(timer)
+  runTimer()
+})
+
+window.addEventListener('scroll', (e) => {
+  clearTimeout(timer)
+  runTimer()
+})
+
+window.addEventListener('keydown', (e) => {
+  clearTimeout(timer)
+  runTimer()
+})
+
+window.addEventListener('keyup', (e) => {
+  clearTimeout(timer)
+  runTimer()
+})
 document.querySelector('#logout').addEventListener('click', logout);
