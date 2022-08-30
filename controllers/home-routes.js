10546d800d7290462774ffee82e2fb48e0620671
Brandon Espinosa <espinbrandon49@gmail.com>
2022-08-29 23:49:13 -0700

    nav reads logout if user is logged in

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -124,31 +124,14 @@ router.get('/blog/comment/:id', async (req, res) => {
           }
         }
       ]
-    // const blogData = await Blog.findOne({
-    //   where: {
-    //     id: req.params.id
-    //   },
-    //   include: [
-    //     {
-    //       model: User,
-    //       attributes: ['username'],
-    //     },
-    //     {
-    //       model: Comment,
-    //       attributes: ['id', 'comment', 'blog_id', 'user_id', 'createdAt'],
-    //       include: {
-    //         model: User,
-    //         attributes:['username']
-    //       }
-    //     }
-    //   ]
     });
 
     const blog = blogData.get({ plain: true });
 
     if (req.session.logged_in) {
       res.render('comment', {
-        ...blog
+        ...blog,
+        logged_in: req.session.logged_in
       })
 
     } else {
