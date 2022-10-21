9a133abe25b23c83b04daf221947a2fa3436c4df
Brandon Espinosa <espinbrandon49@gmail.com>
2022-10-21 15:41:22 -0700

    deploy

--- public/js/logout.js
+++ public/js/logout.js
@@ -13,39 +13,39 @@ const logout = async () => {
 
 let timer;
 
-// const runTimer = () => {
-//   timer = setTimeout(() => {
-//     logout()
-//   }, 60000)
-// }
-// runTimer();
+const runTimer = () => {
+  timer = setTimeout(() => {
+    logout()
+  }, 60000)
+}
+runTimer();
 
-// function resetLogout() {
-//   clearTimeout(myTimeout)
-// }
+function resetLogout() {
+  clearTimeout(myTimeout)
+}
 
-// window.addEventListener('click', (e) => {
-//   clearTimeout(timer)
-//   runTimer()
-// })
+window.addEventListener('click', (e) => {
+  clearTimeout(timer)
+  runTimer()
+})
 
-// window.addEventListener('mousemove', (e) => {
-//   clearTimeout(timer)
-//   runTimer()
-// })
+window.addEventListener('mousemove', (e) => {
+  clearTimeout(timer)
+  runTimer()
+})
 
-// window.addEventListener('scroll', (e) => {
-//   clearTimeout(timer)
-//   runTimer()
-// })
+window.addEventListener('scroll', (e) => {
+  clearTimeout(timer)
+  runTimer()
+})
 
-// window.addEventListener('keydown', (e) => {
-//   clearTimeout(timer)
-//   runTimer()
-// })
+window.addEventListener('keydown', (e) => {
+  clearTimeout(timer)
+  runTimer()
+})
 
-// window.addEventListener('keyup', (e) => {
-//   clearTimeout(timer)
-//   runTimer()
-// })
+window.addEventListener('keyup', (e) => {
+  clearTimeout(timer)
+  runTimer()
+})
 document.querySelector('#logout').addEventListener('click', logout);
